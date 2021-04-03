622e14b03cf40efe94a08535d870b03980236e09
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-04-03 16:22:07 +0200

    feat: added dev mode to console

--- electron-app/src/components/Console.tsx
+++ electron-app/src/components/Console.tsx
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ConsoleLine } from '../helpers/types';
 import { ConsoleContext } from '../contexts/Contexts';
 
 const Console: React.FC = () => {
   const { commandConsole } = useContext(ConsoleContext);
+  const [isDevMode, setIsDevMode] = useState(false)
+
   return (
     <div className="console-wrapper">
-      {commandConsole.map((line: ConsoleLine, i: number) => (
-        <p key={i} style={{ color: line.failed ? 'red' : 'black' }}>
-          {line.text}
-        </p>
-      ))}
+      <div>
+        <button type="button" className="dev-mode-console-button" onClick={() => setIsDevMode(!isDevMode)}>{'<>'}</button>
+      </div>
+      <div className="console-info">
+        {commandConsole.map((line: ConsoleLine, i: number) => (
+          <p key={i} style={{ color: line.failed ? 'red' : 'black' }}>
+            {isDevMode ? line.debugInfo : line.text}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
